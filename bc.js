6c3c6b3e598c0df253ecf3578955d1d0f6306532
yazanzaghal1 <45266019+yazanzaghal1@users.noreply.github.com>
2018-11-23 01:11:00 -0800

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,24 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Simple`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Simple `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`لا سيادة بحضور الملوك`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+ client.login("NDQ5OTUzODUzMDc3NTg1OTI1.DtlM3Q.FHyujWhx-mChPBotpc1FywDg7Ag");
